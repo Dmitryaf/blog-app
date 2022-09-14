b79a3afc3d83da5796cb7b7fb9800fa7f17942e9
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-14 21:24:20 +0300

    Добавил cssLoader

--- src/config/build/buildLoaders.ts
+++ src/config/build/buildLoaders.ts
@@ -6,5 +6,13 @@ export function buildLoaders(): webpack.RuleSetRule[] {
     use: "ts-loader",
     exclude: /node_modules/,
   };
-  return [tsLoader];
+  const cssLoader = {
+        test: /\.s[ac]ss$/i,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader",
+        ],
+      }
+  return [tsLoader, cssLoader];
 }
