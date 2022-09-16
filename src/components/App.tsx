e7d8442f6cb1237f9ca05b9aefb95633e003a624
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-16 17:52:06 +0300

    Добавил тестовые роуты

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import { Test } from "./Test";
+import { AboutPageAsync } from "../pages/About/AboutPageAsync";
+import { MainPageAsync } from "../pages/MainPage/MainPageAsync";
 
 import "./index.scss";
 
 export const App = () => {
   return (
     <div className="app">
-      apppp
-      <Test />
+      <BrowserRouter>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<MainPageAsync />}></Route>
+            <Route path="/about" element={<AboutPageAsync />}></Route>
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
     </div>
   );
 };
