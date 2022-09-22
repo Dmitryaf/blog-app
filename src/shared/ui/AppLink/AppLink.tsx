b89dfbf4e68990dc92b351202abbefe5ec7e03d2
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-22 20:32:24 +0300

    Вынес Link в отдельный shared компонент

---
+++ src/shared/ui/AppLink/AppLink.tsx
@@ -0,0 +1,34 @@
+import { FC } from "react";
+import { Link, LinkProps } from "react-router-dom";
+import { classNames } from "shared/lib/classNames/classNames";
+
+import cls from "./AppLink.module.scss";
+
+export const enum AppLinkTheme {
+  PRIMARY = "primary",
+  SECONDARY = "secondary",
+}
+
+interface AppLinkProps extends LinkProps {
+  className?: string;
+  theme?: string;
+}
+
+export const AppLink: FC<AppLinkProps> = (props) => {
+  const {
+    to,
+    className,
+    children,
+    theme = AppLinkTheme.PRIMARY,
+    ...otherProps
+  } = props;
+  return (
+    <Link
+      to={to}
+      className={classNames(cls.appLink, {}, [className, cls[theme]])}
+      {...otherProps}
+    >
+      {children}
+    </Link>
+  );
+};
