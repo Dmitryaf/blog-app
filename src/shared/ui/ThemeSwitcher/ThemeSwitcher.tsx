f2f541eef4df92a133721027c20e5f87033891e2
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-26 16:58:12 +0300

    Убрал  динамическую смену класса при смене темы

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,17 +1,16 @@
 import { useTheme } from "shared/lib/theme/useTheme";
-
+import { classNames } from "shared/lib/classNames/classNames";
 import ThemeIcon from "shared/assets/theme_icon.svg";
 
 import { Button } from "../Button/Button";
 
 import cls from "./ThemeSwitcher.module.scss";
-import { classNames } from "shared/lib/classNames/classNames";
 
 export const ThemeSwitcher = () => {
   const { theme, toggleTheme } = useTheme();
   return (
     <Button type="button" onClick={() => toggleTheme()}>
-      <ThemeIcon className={classNames("theme-icon", {}, [cls[theme]])} />
+      <ThemeIcon className={classNames("theme-icon", {}, [cls.switcherIcon])} />
     </Button>
   );
 };
