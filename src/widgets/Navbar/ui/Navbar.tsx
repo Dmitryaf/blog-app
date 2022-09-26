878b3e94652ee9681ca07882962432c8c1157900
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-26 16:58:41 +0300

    Вынес свитчер в сайдбар

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,5 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
-import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ThemeSwitcher";
 
 import cls from "./Navbar.module.scss";
 
@@ -11,7 +10,6 @@ interface NavbarProps {
 export const Navbar = ({ classes }: NavbarProps) => {
   return (
     <div className={classNames(cls.navbar, {}, [classes])}>
-      <ThemeSwitcher />
       <div className={cls.links}>
         <AppLink theme={AppLinkTheme.ACTIVE} to="/">
           Home
