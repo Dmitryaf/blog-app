1dd8127d947530b911c9e0ee0984824b3fe9e601
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-27 18:26:56 +0300

    Добавил обертку Suspense после добавления i18n

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,17 +5,19 @@ import { classNames } from "shared/lib/classNames/classNames";
 
 import { Navbar } from "widgets/Navbar";
 import { Sidebar } from "widgets/Sidebar";
+import { Suspense } from "react";
 
 const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames("app", {}, [theme])}>
-      <Navbar />
-
-      <div className="page-content">
-        <Sidebar />
-        <AppRouter />
-      </div>
+      <Suspense fallback="Loading...">
+        <Navbar />
+        <div className="page-content">
+          <Sidebar />
+          <AppRouter />
+        </div>
+      </Suspense>
     </div>
   );
 };
