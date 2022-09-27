6c128c7466a53b75fd99e540933c1ed2f38d25ad
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-27 18:27:34 +0300

    Добавил переключатель языков

---
+++ src/shared/ui/LangSwitcher/LangSwitcher.tsx
@@ -0,0 +1,26 @@
+import { useTranslation } from "react-i18next";
+import { classNames } from "shared/lib/classNames/classNames";
+
+import { Button } from "../Button/Button";
+
+import cls from "./LangSwitcher.module.scss";
+
+interface LangSwitcherProps {
+  classes?: string;
+}
+
+export const LangSwitcher = ({ classes }: LangSwitcherProps) => {
+  const { t, i18n } = useTranslation();
+  const toggleLang = () => {
+    i18n.changeLanguage(i18n.language === "ru" ? "en" : "ru");
+  };
+  return (
+    <Button
+      className={classNames(cls.langSwitcher, {}, [classes])}
+      type="button"
+      onClick={toggleLang}
+    >
+      {t("Язык")}
+    </Button>
+  );
+};
--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
+import { LangSwitcher } from "shared/ui/LangSwitcher/LangSwitcher";
 import { ThemeSwitcher } from "shared/ui/ThemeSwitcher/ThemeSwitcher";
 
 import cls from "./Sidebar.module.scss";
@@ -18,7 +19,7 @@ export const Sidebar = ({ classes }: SidebarProps) => {
     >
       <div className={cls.switchers}>
         <ThemeSwitcher />
-        {/* Lang switcher */}
+        <LangSwitcher classes={cls.lang} />
       </div>
 
       <button onClick={() => setIsCollapsed((prev) => !prev)}>TOGGLE</button>
