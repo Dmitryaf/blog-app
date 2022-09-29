051e9b4e59c80bec063e48a394da42152c8ebec4
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-09-29 18:10:37 +0300

    Create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint"],
+  rules: {
+    "react/jsx-indent": [2, 2],
+    "react/jsx-indent-props": [2, 2],
+    indent: [2, 2],
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".tsx"] },
+    ],
+    "import/no-unresolved": "off",
+    "no-unused-vars": "warn",
+    "react/require-default-props": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-props-no-spreading": "warn",
+    "react/function-component-definition": "off",
+    "no-shadow": "off",
+    "import/extensions": "off",
+    "import/no-extraneous-dependencies": "off",
+    quotes: [2, "double", { avoidEscape: true }],
+    "no-underscore-dangle": "off",
+    "linebreak-style": ["error", "windows"],
+    "operator-linebreak": ["error", "after"],
+    "arrow-body-style": ["error", "as-needed"],
+  },
+  globals: {
+    __IS_DEV__: true,
+  },
+};
