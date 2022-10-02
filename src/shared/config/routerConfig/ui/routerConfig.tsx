ba532b637d1d0fbd192262ae5c8da608d934e1c0
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-02 11:52:10 +0300

    Добавил компонент для несуществующей страницы

--- src/shared/config/routerConfig/ui/routerConfig.tsx
+++ src/shared/config/routerConfig/ui/routerConfig.tsx
@@ -2,18 +2,22 @@ import { RouteProps } from "react-router-dom";
 
 import AboutPage from "pages/AboutPage";
 import MainPage from "pages/MainPage";
+import NotFoundPage from "pages/NotFoundPage/ui/NotFoundPage";
 
 const enum AppRouters {
   MAIN = "main",
   ABOUT = "about",
+  NOT_FOUND = "not_found"
 }
 
 export const RouterPath: Record<AppRouters, string> = {
   [AppRouters.MAIN]: "/",
   [AppRouters.ABOUT]: "/about",
+  [AppRouters.NOT_FOUND]: "*",
 };
 
 export const RouterConfig: RouteProps[] = [
   { path: RouterPath.main, element: <MainPage /> },
   { path: RouterPath.about, element: <AboutPage /> },
+  { path: RouterPath.not_found, element: <NotFoundPage /> },
 ];
