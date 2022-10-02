9b683bf80cac88db1b60d5bf60c4db6374f8f10d
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-02 10:49:49 +0300

    Добавил дефолтные значения

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -2,13 +2,13 @@ type ModsType = Record<string, string | boolean>;
 
 const classNames = (
   mainClass: string,
-  mods?: ModsType,
-  additionClasses?: string[],
+  mods: ModsType = {},
+  additionClasses: string[] = [],
 ): string => [
   mainClass,
   ...additionClasses.filter(Boolean),
-  Object.entries(mods)
-    .filter(([cls, value]) => Boolean(value))
+  ...Object.entries(mods)
+    .filter(([_, value]) => Boolean(value))
     .map(([className]) => className),
 ].join(" ");
 
