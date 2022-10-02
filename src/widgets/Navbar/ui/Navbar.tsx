d9b08fbce0532ef89db02f8c0554fb397393fbed
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-02 10:50:53 +0300

    Добавил переводы для ссылок

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,3 +1,6 @@
+import { useTranslation } from "react-i18next";
+import { RouterPath } from "shared/config/routerConfig/ui/routerConfig";
+
 import classNames from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 
@@ -7,15 +10,18 @@ interface NavbarProps {
   classes?: string;
 }
 
-const Navbar = ({ classes }: NavbarProps) => (
-  <div className={classNames(cls.navbar, {}, [classes])}>
-    <div className={cls.links}>
-      <AppLink theme={AppLinkTheme.ACTIVE} to="/">
-        Home
-      </AppLink>
-      <AppLink to="/about">About</AppLink>
+const Navbar = ({ classes }: NavbarProps) => {
+  const { t, i18n } = useTranslation();
+  return (
+    <div className={classNames(cls.navbar, {}, [classes])}>
+      <div className={cls.links}>
+        <AppLink theme={AppLinkTheme.ACTIVE} to={RouterPath.main}>
+          {t("Главная")}
+        </AppLink>
+        <AppLink to={RouterPath.about}>{t("О нас")}</AppLink>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
