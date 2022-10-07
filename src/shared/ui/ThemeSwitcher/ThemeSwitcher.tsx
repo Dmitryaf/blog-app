442a1b39214ee78bdac098423ba50da9748227b6
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-07 15:38:05 +0300

    Поправил импорты

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@ import useTheme from "shared/lib/theme/useTheme";
 import classNames from "shared/lib/classNames/classNames";
 import ThemeIcon from "shared/assets/theme_icon.svg";
 
-import Button, { ButtonTheme } from "../Button/Button";
+import { Button, ButtonTheme } from "../Button/Button";
 
 import cls from "./ThemeSwitcher.module.scss";
 
