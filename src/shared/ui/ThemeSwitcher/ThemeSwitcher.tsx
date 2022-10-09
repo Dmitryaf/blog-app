75bdf4f22916816de147c087ec7826c41c5504b6
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-09 10:52:54 +0300

    Update ThemeSwitcher.tsx

--- src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/shared/ui/ThemeSwitcher/ThemeSwitcher.tsx
@@ -2,7 +2,7 @@ import useTheme from 'shared/lib/theme/useTheme';
 import classNames from 'shared/lib/classNames/classNames';
 import ThemeIcon from 'shared/assets/theme_icon.svg';
 
-import { Button, ButtonTheme } from '../Button/Button';
+import { Button } from '../Button/Button';
 
 import cls from './ThemeSwitcher.module.scss';
 
@@ -15,9 +15,8 @@ const ThemeSwitcher = ({ classes }: ThemeSwitcherProps) => {
   return (
     <Button
       className={classNames(cls.themeSwitcher, {}, [classes])}
-      theme={ButtonTheme.CLEAR}
       type="button"
-      onClick={() => toggleTheme()}
+      onClick={toggleTheme}
     >
       <ThemeIcon className={classNames('theme-icon', {}, [cls.switcherIcon])} />
     </Button>
