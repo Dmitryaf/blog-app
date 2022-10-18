ce7570b6e8cdeeec144581ae6ad7ae63bdef906b
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-18 19:26:51 +0300

    Вынес моды в отдельную переменную. Добавил размеры

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -8,21 +8,29 @@ export const enum ButtonTheme {
   OUTLINE = 'outline',
   CLEAR = 'clear',
   BACKGROUND = 'background',
+  BACKGROUND_INVERTED = 'backgroundInverted',
+}
+
+export const enum ButtonSize {
+  L = 'size_l',
+  XL = 'size_xl',
 }
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   className?: string;
   theme?: string;
+  size?: string;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
   const {
-    className, children, theme = '', ...otherProps
+    className, children, theme = '', size = '', ...otherProps
   } = props;
+  const mods: Record<string, boolean> = { [cls[theme]]: true, [cls[size]]: true };
   return (
     <button
       type="button"
-      className={classNames(cls.button, {}, [className, cls[theme]])}
+      className={classNames(cls.button, mods, [className])}
       {...otherProps}
     >
       {children}
