73319bd9da5bcd38b2c558db3cf20c850111a4ff
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-10-26 18:22:14 +0300

    Добавил кнопку для авторизации

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,8 +1,12 @@
+/* eslint-disable i18next/no-literal-string */
+import { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { RouterPath } from 'shared/config/routerConfig/ui/routerConfig';
 
 import classNames from 'shared/lib/classNames/classNames';
-import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { Button } from 'shared/ui/Button/Button';
+
+import Modal from 'shared/ui/Modal/Modal';
+import Portal from 'shared/ui/Portal/Portal';
 
 import cls from './Navbar.module.scss';
 
@@ -11,9 +15,24 @@ interface NavbarProps {
 }
 
 const Navbar = ({ className }: NavbarProps) => {
-  const { t, i18n } = useTranslation();
+  const { t } = useTranslation();
+  const [isOpenAuthModal, setIsOpenAuthModal] = useState(false);
+
+  const onToggleModal = useCallback(() => {
+    setIsOpenAuthModal((prev) => !prev);
+  }, []);
   return (
-    <div className={classNames(cls.navbar, {}, [className])} />
+    <div className={classNames(cls.navbar, {}, [className])}>
+      <Button onClick={onToggleModal}>{t('Войти')}</Button>
+      <Portal>
+        <Modal isOpen={isOpenAuthModal} onClose={onToggleModal}>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          Excepturi voluptates fugit nam quam doloribus totam, minus saepe nobis ex a,
+          rem placeat inventore quos voluptatibus iure, quisquam doloremque suscipit corrupti?
+        </Modal>
+      </Portal>
+    </div>
+
   );
 };
 
