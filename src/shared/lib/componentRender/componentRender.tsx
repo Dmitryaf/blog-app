b699258f8f26e3996995bd85231dc154a93f7e87
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-11-03 18:11:10 +0300

    Добавил провайдер для редакса

--- src/shared/lib/componentRender/componentRender.tsx
+++ src/shared/lib/componentRender/componentRender.tsx
@@ -1,24 +1,32 @@
 import { ReactNode } from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
+import { DeepPartial } from '@reduxjs/toolkit';
+
 import { render } from '@testing-library/react';
 
 import { I18nextProvider } from 'react-i18next';
 
+import { StateSchema } from 'shared/config/redux/StateSchema';
 import i18nForTests from 'shared/config/i18n/i18nForTests';
+
+import StoreProvider from 'app/providers/StoreProvider';
 
 export interface componentRenderOption {
   route?: string,
+  initialState?: DeepPartial<StateSchema>
 }
 
 const componentRender = (component: ReactNode, options: componentRenderOption = {}) => {
-  const { route = '/' } = options;
+  const { route = '/', initialState } = options;
   return render(
-    <MemoryRouter initialEntries={[route]}>
-      <I18nextProvider i18n={i18nForTests}>
-        {component}
-      </I18nextProvider>
-    </MemoryRouter>,
+    <StoreProvider initialState={initialState}>
+      <MemoryRouter initialEntries={[route]}>
+        <I18nextProvider i18n={i18nForTests}>
+          {component}
+        </I18nextProvider>
+      </MemoryRouter>
+    </StoreProvider>,
   );
 };
 
