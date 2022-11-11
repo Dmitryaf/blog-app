72f3a4eb57bf19309832c1f9d76bd416b35e4b38
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-11-11 18:07:50 +0300

    Доработал логику передачи редьюсеров

--- src/shared/config/redux/strore.ts
+++ src/shared/config/redux/strore.ts
@@ -1,13 +1,21 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, ReducersMapObject } from '@reduxjs/toolkit';
+import { userReducer } from 'entities/User';
 
 import { counterReducer } from 'features/Counter/model/slice/counterSlice';
 
 import { StateSchema } from './StateSchema';
 
-const createReduxStore = (initialState: StateSchema) => configureStore({
-  reducer: { counter: counterReducer },
-  devTools: __IS_DEV__,
-  preloadedState: initialState,
-});
+const createReduxStore = (initialState: StateSchema) => {
+  const rootReducers: ReducersMapObject<StateSchema> = {
+    counter: counterReducer,
+    user: userReducer,
+  };
+
+  return configureStore({
+    reducer: rootReducers,
+    devTools: __IS_DEV__,
+    preloadedState: initialState,
+  });
+};
 
 export default createReduxStore;
