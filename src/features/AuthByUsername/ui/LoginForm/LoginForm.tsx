83052423735247fbe31219558fd16b3cbf9041f4
Dmitry Afonasenko <dmitrytbp@gmail.com>
2022-11-15 19:55:40 +0300

    Доработал форму авторизации

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 
 import classNames from 'shared/lib/classNames/classNames';
-import { Button } from 'shared/ui/Button/Button';
-import Input from 'shared/ui/Input/Input';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import Input, { InputLabelTheme } from 'shared/ui/Input/Input';
 
 import cls from './LoginForm.module.scss';
 
@@ -15,9 +15,19 @@ const LoginForm = ({ className }: LoginFormProps) => {
 
   return (
     <div className={classNames(cls.loginForm, {}, [className])}>
-      {/* <Input className={cls.input} />
-      <Input className={cls.input} /> */}
-      <Button className={cls.loginBtn}>{t('Войти')}</Button>
+      <Input
+        classNameWrapper={cls.input}
+        label={t('Логин')}
+        labelTheme={InputLabelTheme.INVERTED}
+        isLabelTop
+      />
+      <Input
+        classNameWrapper={cls.input}
+        label={t('Пароль')}
+        labelTheme={InputLabelTheme.INVERTED}
+        isLabelTop
+      />
+      <Button className={cls.loginBtn} theme={ButtonTheme.BACKGROUND}>{t('Войти')}</Button>
     </div>
 
   );
